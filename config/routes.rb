73ff7ebd26444e98d1071662a95a7b855cb3b44a
Milena Gaigne <milena.gaigne@gmail.com>
2019-02-11 16:17:42 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
+  get 'ask', to: 'questions_controller#ask', as: :ask
+  get 'answer', to: 'questions_controller#answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'ask', to: 'questions#ask'
-  get 'answer', to: 'questions#answer'
 end
